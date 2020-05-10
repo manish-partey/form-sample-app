cd0a54f7da8ae6b2ab2466fa9f3bf1151eb68f6e
Partey, Manish <manish.partey@duckcreek.com>
2020-05-10 20:35:02 +0530

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import{ MatInputModule} from '@angular/material/input';
+import{ MatCardModule} from '@angular/material/card';
+import{ MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import {PostCreateComponent} from './posts/post-create/post-create.component';
 @NgModule({
   declarations: [
@@ -15,7 +18,9 @@ import {PostCreateComponent} from './posts/post-create/post-create.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    MatInputModule,MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
